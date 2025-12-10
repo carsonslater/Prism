08185b73a3c14c112e9cf62468dbc78cc9eb1ef3
Carson Slater <carsonslater7@gmail.com>
2025-12-09 20:40:03 -0600

    first migrations

--- FeedApp/models.py
+++ FeedApp/models.py
@@ -2,21 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# The image shows a Django model definition for a User Profile
 class Profile(models.Model):
     first_name = models.CharField(max_length=200, blank=True)
     last_name = models.CharField(max_length=200, blank=True)
     email = models.EmailField(max_length=300, blank=True)
     dob = models.DateField(null=True, blank=True)
     bio = models.TextField(blank=True)
-    # Establishes a 1-to-1 link with Django's built-in User model
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # Establishes a Many-to-Many link with the User model for friends
     friends = models.ManyToManyField(User, blank=True, related_name="friends")
-    # Automatically set when the object is first created
-    created = models.DateTimeField(auto_now=True)
-    # Automatically set every time the object is saved
-    updated = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
 
 
 STATUS_CHOICES = (("sent", "sent"), ("accepted", "accepted"))
@@ -27,9 +25,9 @@ class Relationship(models.Model):
     receiver = models.ForeignKey(
         Profile, on_delete=models.CASCADE, related_name="receiver"
     )
-    status = models.CharField(max_length=8, choices=STATUS_CHOICES, default="send")
-    created = models.DateTimeField(auto_now=True)
-    updated = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES, default="sent")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
 
 class Post(models.Model):
--- FeedApp/views.py
+++ FeedApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import PostForm,ProfileForm, RelationshipForm
+from .forms import PostForm, ProfileForm, RelationshipForm
 from .models import Post, Comment, Like, Profile, Relationship
 from datetime import datetime, date
 
@@ -9,17 +9,13 @@ from django.http import Http404
 
 # Create your views here.
 
-# When a URL request matches the pattern we just defined, 
-# Django looks for a function called index() in the views.py file. 
+# When a URL request matches the pattern we just defined,
+# Django looks for a function called index() in the views.py file.
+
 
 def index(request):
     """The home page for Learning Log."""
-    return render(request, 'FeedApp/index.html')
+    return render(request, "FeedApp/index.html")
 
 
-
-#@login_required
-
-
-
-
+# @login_required
